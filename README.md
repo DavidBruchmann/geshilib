aae711cf09fff1dd9bf0b711e7f01249a780ecf5
David Bruchmann <david.bruchmann@gmail.com>
2017-08-12 04:25:05 +0700

    Version 0.0.1 of the TYPO3-extension geshilib

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # geshilib
-Provides a library for use in extensions. GeSHi is a syntax highlighter for HTML, written in PHP. Basically, you input the source you want to highlight and the name of the language you want to highlight it in, and GeSHi returns the syntax-highlighted result (http://qbnz.com/highlighter/index.php).
+Provides a library for use in extensions. GeSHi is a syntax highlighter for HTML, written in PHP.  
+Basically, you input the source you want to highlight and the name of the language you want to highlight it in, and GeSHi returns the syntax-highlighted result (http://qbnz.com/highlighter/index.php).  
+
+This extension was created by Vincent Tietz <vincent.tietz@vj-media.de> but outdated for currrent versions of TYPO3.  
+This document is related to version 0.0.1 of the extension.  
+License:  [GNU General Public License version 2 or later](http://www.gnu.org/copyleft/gpl.html).  
---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "geshilib".
+#
+# Auto generated 31-08-2015 14:05
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'GeSHi - Generic Syntax Highlighter (lib)',
+	'description' => 'Provides a library for use in extensions. GeSHi is a syntax highlighter for HTML, written in PHP. Basically, you input the source you want to highlight and the name of the language you want to highlight it in, and GeSHi returns the syntax-highlighted result (http://qbnz.com/highlighter/index.php).',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Vincent Tietz',
+	'author_email' => 'vincent.tietz@vj-media.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
